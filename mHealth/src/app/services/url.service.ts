c7ba119db114d18bc6a5b78f17529112c6cd9d5b
Max <max@mb-media.net>
2021-12-11 15:47:39 +0100

    remove hard coded IP

--- mHealth/src/app/services/url.service.ts
+++ mHealth/src/app/services/url.service.ts
@@ -15,7 +15,7 @@ export class UrlService {
     const protocol = window.location.protocol;
     let hostname = window.location.hostname;
     if(this.platformService.isApp()){
-      hostname = '192.168.2.30';
+      //hostname = '192.168.2.30';
     }
     // eslint-disable-next-line no-console
     console.debug(`${protocol}//${hostname}:${this.port}/${path}`);
